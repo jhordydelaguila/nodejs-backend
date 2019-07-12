6f582bf607c84baff0b4eac6b80d04cf1dd6b496
Jhordy Alexi <jhordydelaguila@hotmail.com>
2019-07-12 11:45:08 -0500

    Add express

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const math = require('./math.js');
 
 const colors = require('colors');
 
+/*
 const http = require('http');
 
 const handleServer = function (req, res) {
@@ -14,4 +15,18 @@ const server = http.createServer(handleServer);
 
 server.listen(3000, function () {
     console.log("Server on port 3000".blue);
+});
+*/
+
+const express = require('express');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.send("<h1>Hello word with Express y nodejs </h1>");
+    res.end();
+});
+
+server.listen(3000, () => {
+    console.log("Server on port 3000".red);
 });
